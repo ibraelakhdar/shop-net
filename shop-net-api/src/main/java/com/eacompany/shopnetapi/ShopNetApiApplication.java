076641f9797deb06f9900ced1366cc73c0ccdd10
elakhdaribrahim1 <ielakhdar@gmail.com>
2019-11-07 22:45:55 +0100

    chore (shop-net) : init the project
    add poms of parents, java-service-parent
    init shop-net-api

---
+++ shop-net-api/src/main/java/com/eacompany/shopnetapi/ShopNetApiApplication.java
@@ -0,0 +1,13 @@
+package com.eacompany.shopnetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopNetApiApplication.class, args);
+	}
+
+}
---
+++ shop-net-api/src/test/java/com/eacompany/shopnetapi/ShopNetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacompany.shopnetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopNetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
